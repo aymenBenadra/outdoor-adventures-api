ddedfa4668bb7c1d798464ba719632c1547130de
IOU4 <ouchaibimad000@gmail.com>
2023-02-09 09:46:07 +0100

    rename app

--- customer/src/main/java/ucode/outdoorcustomer/OutdoorCustomerApplication.java
+++ customer/src/main/java/ucode/outdoorcustomer/OutdoorCustomerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class OutdoorCustomerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+    SpringApplication.run(OutdoorCustomerApplication.class, args);
   }
 
 }
