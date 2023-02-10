3de7fb14ff1416c74dc031b57f47d0176938411c
IOU4 <ouchaibimad000@gmail.com>
2023-02-10 10:04:51 +0100

    initial cart service

---
+++ cart/src/test/java/ucode/outdoorshoppingcart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package ucode.outdoorshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
