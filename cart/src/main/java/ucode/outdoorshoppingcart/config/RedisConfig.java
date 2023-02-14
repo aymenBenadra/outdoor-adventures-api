a278e2ffd77addea6fbc4e6e4ec96d3617a42b7a
IOU4 <ouchaibimad000@gmail.com>
2023-02-14 11:50:10 +0100

    update config to use default values

--- cart/src/main/java/ucode/outdoorshoppingcart/config/RedisConfig.java
+++ cart/src/main/java/ucode/outdoorshoppingcart/config/RedisConfig.java
@@ -3,21 +3,22 @@ package ucode.outdoorshoppingcart.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
+
   @Bean
   public JedisConnectionFactory jedisConnectionFactory() {
-      var config = new RedisStandaloneConfiguration("localhost", 6379);
-    return new JedisConnectionFactory(config);
+    return new JedisConnectionFactory();
   }
 
   @Bean
-  public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-    var template = new StringRedisTemplate(redisConnectionFactory);
+  public StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    var template = new StringRedisTemplate();
+    template.setConnectionFactory(redisConnectionFactory);
     return template;
   }
 }
