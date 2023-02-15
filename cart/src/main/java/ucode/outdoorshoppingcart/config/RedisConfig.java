6d9c969c9e0f1205e8e1266855e277b6d25a0c3e
IOU4 <ouchaibimad000@gmail.com>
2023-02-15 09:32:04 +0100

    use Map datatype for redisTemplate

--- cart/src/main/java/ucode/outdoorshoppingcart/config/RedisConfig.java
+++ cart/src/main/java/ucode/outdoorshoppingcart/config/RedisConfig.java
@@ -1,11 +1,12 @@
 package ucode.outdoorshoppingcart.config;
 
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
@@ -16,8 +17,9 @@ public class RedisConfig {
   }
 
   @Bean
-  public StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-    var template = new StringRedisTemplate();
+  public RedisTemplate<String, Map<String, Integer>> redisTemplate(
+      RedisConnectionFactory redisConnectionFactory) {
+    var template = new RedisTemplate<String, Map<String, Integer>>();
     template.setConnectionFactory(redisConnectionFactory);
     return template;
   }
