1330f571ae09dba474bd1a12f16a201f34638324
IOU4 <ouchaibimad000@gmail.com>
2023-02-15 09:33:34 +0100

    getall and addtocart resouces with associated services

--- cart/src/main/java/ucode/outdoorshoppingcart/CartResource.java
+++ cart/src/main/java/ucode/outdoorshoppingcart/CartResource.java
@@ -1,8 +1,14 @@
 package ucode.outdoorshoppingcart;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +29,21 @@ public class CartResource {
   }
 
   @GetMapping(value = "/{cid}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Cart getCartitems(@PathVariable Long cid) {
-    return cartService.getCartItems(cid);
+  public Cart getCartitems(@PathVariable long cid) {
+    return cartService.getCartItems("cid:" + cid);
   }
+
+  @PostMapping("/{id}")
+  public ResponseEntity<?> addToCart(@PathVariable("id") Long cid, @RequestBody AddToCartRequest addToCartRequest) {
+    cartService.addToCart("cid:" + cid, "pid:" + addToCartRequest.pid(), addToCartRequest.quantity());
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @GetMapping("/all")
+  public ResponseEntity<List<Cart>> getAllCarts() {
+    return ResponseEntity.ok(cartService.getAllCarts());
+  }
+}
+
+record AddToCartRequest(String pid, int quantity) {
 }
--- cart/src/main/java/ucode/outdoorshoppingcart/CartService.java
+++ cart/src/main/java/ucode/outdoorshoppingcart/CartService.java
@@ -1,7 +1,10 @@
 package ucode.outdoorshoppingcart;
 
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.support.atomic.RedisAtomicLong;
 import org.springframework.stereotype.Service;
 
@@ -14,24 +17,38 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class CartService {
 
-  private final StringRedisTemplate redisTemplate;
-  private final ListOperations<String, String> listOps;
+  private final RedisTemplate<String, Map<String, Integer>> redisTemplate;
+  private final HashOperations<String, String, Integer> hashOps;
   private final RedisAtomicLong cartIdCounter;
+  private final CartRepository cartRepository;
 
-  public CartService(StringRedisTemplate redisTemplate) {
+  public CartService(RedisTemplate<String, Map<String, Integer>> redisTemplate, CartRepository cartRepository) {
     this.redisTemplate = redisTemplate;
-    this.listOps = redisTemplate.opsForList();
     this.cartIdCounter = new RedisAtomicLong("globla:cid", redisTemplate.getConnectionFactory());
+    this.hashOps = redisTemplate.opsForHash();
+    this.cartRepository = cartRepository;
   }
 
   public Long createCart() {
     long cid = cartIdCounter.incrementAndGet();
     log.info("new cart id: " + cid);
-    listOps.leftPush("cid:" + cid, "null");
+    hashOps.put("cid:" + cid, "null", 0);
     return cid;
   }
 
-  public Cart getCartItems(Long cid) {
-    return Cart.builder().cid(cid).items(listOps.range("cid:" + cid, 0, -1)).build();
+  public Cart getCartItems(String cid) {
+    return Cart.builder().cid(cid).items(hashOps.entries(cid)).build();
+  }
+
+  public void addToCart(String cid, String pid, int quantity) {
+    if (!redisTemplate.hasKey(cid)) {
+      log.warn("cart with id " + cid + " not found");
+      return;
+    }
+    hashOps.put(cid, pid, quantity);
+  }
+
+  public List<Cart> getAllCarts() {
+    return cartRepository.findAll();
   }
 }
